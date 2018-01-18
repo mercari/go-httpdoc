ae946783f9181740073dd93acaf81be15d03c532
qwerty2501 <oikawa@sinewave.co.jp>
2018-01-18 12:48:49 +0900

    modified by adding AssertFunc to TestCase.

--- validate_test.go
+++ validate_test.go
@@ -71,16 +71,16 @@ func TestValidator_RequestParams(t *testing.T) {
 	}
 
 	validator.RequestParams(t, []TestCase{
-		{"token", "12345", ""},
-		{"pretty", "true", ""},
+		{"token", "12345", "", nil},
+		{"pretty", "true", "", nil},
 	})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.RequestParams(t, []TestCase{
-		{"token", "8976", ""},
-		{"pretty", "", ""},
-		{"id", "u8988", ""},
+		{"token", "8976", "", nil},
+		{"pretty", "", "", nil},
+		{"id", "u8988", "", nil},
 	})
 	if want := 3; got != want {
 		t.Fatalf("expect valiate fails %d, got %d", want, got)
@@ -95,17 +95,17 @@ func TestValidator_RequestHeaders(t *testing.T) {
 		"X-API-Version": []string{"1.1.2"},
 	}
 	validator.RequestHeaders(t, []TestCase{
-		{"User-Agent", "Googlebot/2.1", ""},
-		{"Content-Type", "application/json", ""},
-		{"X-API-Version", "1.1.2", ""},
+		{"User-Agent", "Googlebot/2.1", "", nil},
+		{"Content-Type", "application/json", "", nil},
+		{"X-API-Version", "1.1.2", "", nil},
 	})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.RequestHeaders(t, []TestCase{
-		{"User-Agent", []string{"curl"}, ""},
-		{"Content-Type", []string{"application/protobuf"}, ""},
-		{"X-API-Version", []string{"3.0"}, ""},
+		{"User-Agent", []string{"curl"}, "", nil},
+		{"Content-Type", []string{"application/protobuf"}, "", nil},
+		{"X-API-Version", []string{"3.0"}, "", nil},
 	})
 	if want := 3; got != want {
 		t.Fatalf("expect valiate fails %d, got %d", want, got)
@@ -114,7 +114,7 @@ func TestValidator_RequestHeaders(t *testing.T) {
 	var buf bytes.Buffer
 	tFatalf = fprintFatalFunc(&buf)
 	validator.RequestHeaders(t, []TestCase{
-		{"Not-Found", []string{""}, ""},
+		{"Not-Found", []string{""}, "", nil},
 	})
 
 	if got, want := buf.String(), "not found"; !strings.Contains(got, want) {
@@ -129,14 +129,14 @@ func TestValidator_ResponseHeaders(t *testing.T) {
 		"X-API-Version": []string{"1.1.2"},
 	}
 	validator.ResponseHeaders(t, []TestCase{
-		{"Content-Type", "application/json", ""},
-		{"X-API-Version", "1.1.2", ""},
+		{"Content-Type", "application/json", "", nil},
+		{"X-API-Version", "1.1.2", "", nil},
 	})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.ResponseHeaders(t, []TestCase{
-		{"Content-Type", []string{"application/protobuf"}, ""},
+		{"Content-Type", []string{"application/protobuf"}, "", nil},
 	})
 	if want := 1; got != want {
 		t.Fatalf("expect valiate fails %d, got %d", want, got)
@@ -145,7 +145,7 @@ func TestValidator_ResponseHeaders(t *testing.T) {
 	var buf bytes.Buffer
 	tFatalf = fprintFatalFunc(&buf)
 	validator.ResponseHeaders(t, []TestCase{
-		{"Not-Found", []string{""}, ""},
+		{"Not-Found", []string{""}, "", nil},
 	})
 
 	if got, want := buf.String(), "not found"; !strings.Contains(got, want) {
@@ -163,16 +163,16 @@ func TestValidator_RequestBody(t *testing.T) {
 }
 `)
 	validator.RequestBody(t, []TestCase{
-		{"ID", 910, ""},
-		{"Setting.Email", "taichi@mercari.com", ""},
+		{"ID", 910, "", nil},
+		{"Setting.Email", "taichi@mercari.com", "", nil},
 	}, &User{})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.RequestBody(t, []TestCase{
-		{"ID", 123, ""},
-		{"Active", true, ""},
-		{"Setting.Email", "deeeet@gmail.com", ""},
+		{"ID", 123, "", nil},
+		{"Active", true, "", nil},
+		{"Setting.Email", "deeeet@gmail.com", "", nil},
 	}, &User{})
 
 	if want := 3; got != want {
@@ -202,21 +202,21 @@ func TestValidator_ResponseBody(t *testing.T) {
 }
 `)
 	validator.ResponseBody(t, []TestCase{
-		{"ID", 789, ""},
-		{"Active", false, ""},
-		{"Setting.Email", "tcnksm@mercari.com", ""},
-		{"Permission[1]", "read", ""},
-		{`Preference["email"]`, 0, ""},
+		{"ID", 789, "", nil},
+		{"Active", false, "", nil},
+		{"Setting.Email", "tcnksm@mercari.com", "", nil},
+		{"Permission[1]", "read", "", nil},
+		{`Preference["email"]`, 0, "", nil},
 	}, &User{})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.ResponseBody(t, []TestCase{
-		{"ID", 123, ""},
-		{"Active", true, ""},
-		{"Setting.Email", "deeeet@gmail.com", ""},
-		{"Permission[1]", "write", ""},
-		{`Preference["email"]`, 1, ""},
+		{"ID", 123, "", nil},
+		{"Active", true, "", nil},
+		{"Setting.Email", "deeeet@gmail.com", "", nil},
+		{"Permission[1]", "write", "", nil},
+		{`Preference["email"]`, 1, "", nil},
 	}, &User{})
 
 	if want := 5; got != want {
@@ -250,13 +250,13 @@ func TestValidateFields(t *testing.T) {
 	}
 	validator := newValidator()
 	validator.validateFields(t, []TestCase{
-		{"ID", 12345, ""},
-		{"Name", "tcnksm", ""},
-		{"Active", true, ""},
-		{"Setting.Email", "tcnksm@example.com", ""},
-		{"Setting.SNS.Twitter", "@deeeet", ""},
-		{"Permission[0]", "write", ""},
-		{`Preference["email"]`, 0, ""},
+		{"ID", 12345, "", nil},
+		{"Name", "tcnksm", "", nil},
+		{"Active", true, "", nil},
+		{"Setting.Email", "tcnksm@example.com", "", nil},
+		{"Setting.SNS.Twitter", "@deeeet", "", nil},
+		{"Permission[0]", "write", "", nil},
+		{`Preference["email"]`, 0, "", nil},
 	}, testUser, &[]Data{})
 }
 
@@ -273,14 +273,14 @@ func TestValidator_RequestBody_Proto(t *testing.T) {
 	validator.record.requestBody = buf
 	validator.unmarshalFunc = protoUnmarshalFunc
 	validator.RequestBody(t, []TestCase{
-		{"Id", int32(12345), ""},
-		{"Name", "tcnksm", ""},
+		{"Id", int32(12345), "", nil},
+		{"Name", "tcnksm", "", nil},
 	}, &UserProtoRequest{})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.RequestBody(t, []TestCase{
-		{"Id", 123, ""},
+		{"Id", 123, "", nil},
 	}, &UserProtoRequest{})
 
 	if want := 1; got != want {
@@ -303,15 +303,15 @@ func TestValidator_ResponseBody_Proto(t *testing.T) {
 	validator.unmarshalFunc = protoUnmarshalFunc
 	validator.record.responseBody = buf
 	validator.ResponseBody(t, []TestCase{
-		{"Id", int32(667854), ""},
-		{"Setting.Email", "httpdoc@example.com", ""},
+		{"Id", int32(667854), "", nil},
+		{"Setting.Email", "httpdoc@example.com", "", nil},
 	}, &UserProtoResponse{})
 
 	var got int
 	validator.assertFunc = testAssertWithCount(&got)
 	validator.ResponseBody(t, []TestCase{
-		{"Id", 123, ""},
-		{"Setting.Email", "deeeet@gmail.com", ""},
+		{"Id", 123, "", nil},
+		{"Setting.Email", "deeeet@gmail.com", "", nil},
 	}, &UserProtoResponse{})
 
 	if want := 2; got != want {
